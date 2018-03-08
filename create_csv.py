367daec588b0624e4984d356abe44fd5bfbdcc6f
LeChosenOne <jonahwasserman97@gmail.com>
2018-03-08 18:13:47 -0500

    Create sample graphs with data

--- create_csv.py
+++ create_csv.py
@@ -8,9 +8,10 @@ import os
 os.environ['MOZ_HEADLESS'] = '1'
 binary = FirefoxBinary('/usr/bin/firefox')
 driver = webdriver.Firefox(firefox_binary=binary)
-total_csv = 'Rk,Player,Pos,Age,Tm,G,MP,PER,TS%,3PAr,FTr,ORB%,DRB%,TRB%,AST%,STL%,BLK%,TOV%,USG%,,OWS,DWS,WS,WS/48,,OBPM,DBPM,BPM,VORP,Year\n'
+advanced_csv = 'Rk,Player,Pos,Age,Tm,G,MP,PER,TS%,3PAr,FTr,ORB%,DRB%,TRB%,AST%,STL%,BLK%,TOV%,USG%,,OWS,DWS,WS,WS/48,,OBPM,DBPM,BPM,VORP,Year\n'
+per_game_csv = 'Rk,Player,Pos,Age▼,Tm,G,GS,MP,FG,FGA,FG%,3P,3PA,3P%,2P,2PA,2P%,eFG%,FT,FTA,FT%,ORB,DRB,TRB,AST,STL,BLK,TOV,PF,PS/G,Year\n'
 
-#iterate all recorded seasons
+#iterate all advanced stats
 for year in range(1950,2018):
     driver.get('https://www.basketball-reference.com/leagues/NBA_'+str(year)+'_advanced.html')
     driver.execute_script('document.querySelector("#advanced_stats_toggle_partial_table").click()')
@@ -20,9 +21,24 @@ for year in range(1950,2018):
     #add year to each row, clean up formatting
     csv = csv.replace('\n', ',' + str(year) + '\n')
     csv +=  ',' + str(year) + '\n'
-    total_csv += csv
+    advanced_csv += csv
+
+#iterate all per_game stats
+for year in range(1950,2018):
+    driver.get('https://www.basketball-reference.com/leagues/NBA_'+str(year)+'_per_game.html')
+    driver.execute_script('document.querySelector("#per_game_stats_toggle_partial_table").click()')
+    driver.execute_script('document.querySelector("#all_per_game_stats > div:nth-child(1) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > button:nth-child(1)").click()')
+    csv = driver.find_element_by_css_selector("#csv_per_game_stats").text.split('\n',1)[-1]
+
+    #add year to each row, clean up formatting
+    csv = csv.replace('\n', ',' + str(year) + '\n')
+    csv +=  ',' + str(year) + '\n'
+    per_game_csv += csv
 
 #write to csv
-stats = open("player_stats.csv", "w")
-stats.write(total_csv)
+stats = open("advanced_stats.csv", "w")
+stats.write(advanced_csv)
+stats.close()
+stats = open("per_game_stats.csv", "w")
+stats.write(per_game_csv)
 stats.close()
--- nbastats.py
+++ nbastats.py
@@ -2,39 +2,105 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-from nba_py import shotchart, player
-from scipy.interpolate import spline
+import sys
+import math
+from sklearn.cluster import KMeans
 
-dataframe = pd.read_csv('player_stats.csv')
-number_centers = []
-number_point_guards = []
+adv_df = pd.read_csv('advanced_stats.csv')
+per_game_df = pd.read_csv('per_game_stats.csv').drop(columns=['Rk', 'Tm', 'Pos', 'G', 'MP'])
+dataframe = adv_df.merge(per_game_df, on = ['Player','Year'], how = 'outer')
+dataframe.to_csv('all_stats.csv', index=False)
+
+unlabelled_data = dataframe.drop(columns=['Player', 'Rk', 'Pos', 'Tm', 'Year'])
+kmeans = KMeans(n_clusters=5).fit(unlabelled_data.values)
+
+OWS_centers = []
+OWS_point_guards = []
+OWS_sforwards = []
+OWS_pforwards = []
+OWS_shooting_guards = []
 years = []
 
-#load offensive win shares for each position
-for year in range(1950,2018):
-    df = dataframe[(dataframe['Year'] == year)]
-    top_10_winshares = df.nlargest(10, 'OWS')
-    number_centers.append(len(top_10_winshares[top_10_winshares['Pos'] == 'C']))
-    number_point_guards.append(len(top_10_winshares[top_10_winshares['Pos'] == 'PG']))
-    years.append(year)
+DWS_centers = []
+DWS_pgs = []
 
-#smooth out y values
-years_smooth = np.linspace(1950,2018,10000)
-guards_smooth = spline(years,number_point_guards,years_smooth)
-centers_smooth = spline(years,number_centers,years_smooth)
-plt.plot(years_smooth, centers_smooth, label="Centers")
-plt.plot(years_smooth, guards_smooth, label="Point Guards")
+#load offensive win shares for each position
+year = 1950
+while year <= 2018:
+    centers = dataframe[(dataframe['Year'] == year) & (dataframe['Pos_x'] == 'C')]
+    point_guards = dataframe[(dataframe['Year'] == year) & (dataframe['Pos_x'] == 'PG')]
+    shooting_guards = dataframe[(dataframe['Year'] == year) & (dataframe['Pos_x'] == 'SG')]
+    small_forwards = dataframe[(dataframe['Year'] == year) & (dataframe['Pos_x'] == 'SF')]
+    power_forwards = dataframe[(dataframe['Year'] == year) & (dataframe['Pos_x'] == 'PF')]
+
+    sample_size = 10
+    rating = 'WS'
+    top_centers = centers.nlargest(sample_size, rating)
+    top_pgs = point_guards.nlargest(sample_size, rating)
+    top_sgs = shooting_guards.nlargest(sample_size, rating)
+    top_sfor = small_forwards.nlargest(sample_size, rating)
+    top_pfor = power_forwards.nlargest(sample_size, rating)
+
+    center_ave_OWS = top_centers['OWS'].sum()/sample_size
+    point_guard_ave_OWS = top_pgs['OWS'].sum()/sample_size
+    shooting_guard_ave_OWS = top_sgs['OWS'].sum()/sample_size
+    small_forward_ave_OWS = top_sfor['OWS'].sum()/sample_size
+    power_forward_ave_OWS = top_pfor['OWS'].sum()/sample_size
+
+
+    OWS_centers.append(center_ave_OWS)
+    OWS_point_guards.append(point_guard_ave_OWS)
+    OWS_shooting_guards.append(shooting_guard_ave_OWS)
+    OWS_sforwards.append(small_forward_ave_OWS)
+    OWS_pforwards.append(power_forward_ave_OWS)
+
+    sample_size = 10
+    rating = 'PER'
+    top_centers = centers.nlargest(sample_size, rating)
+    top_pgs = point_guards.nlargest(sample_size, rating)
+    center_ave_DWS = top_centers['DWS'].sum()/sample_size
+    point_guard_ave_DWS = top_pgs['DWS'].sum()/sample_size
+
+    DWS_centers.append(center_ave_DWS)
+    DWS_pgs.append(point_guard_ave_DWS)
+    years.append(year)
+    year += 5
+
+
+#plot each position
+plt.subplot(2, 1, 1)
+plt.plot(years, OWS_centers, label="Centers", linewidth=2)
+plt.plot(years, OWS_point_guards, label="Point Guards", linewidth=2)
+#plt.plot(years, OWS_shooting_guards, label="Shooting Guards", linewidth=2)
+#plt.plot(years, OWS_sforwards, label="Small Forwards", linewidth=2)
+#plt.plot(years, OWS_pforwards, label="Power Forwards", linewidth=2)
+
 
 #graph trendlines
-guards_z = np.polyfit(years_smooth, guards_smooth, 1)
-centers_z = np.polyfit(years_smooth, centers_smooth, 1)
-pg = np.poly1d(guards_z)
-pc = np.poly1d(centers_z)
-plt.plot(years,pg(years),"r:")
-plt.plot(years,pc(years),"b:")
+c = np.poly1d(np.polyfit(years, OWS_centers, 1))
+pg = np.poly1d(np.polyfit(years, OWS_point_guards, 1))
+sg = np.poly1d(np.polyfit(years, OWS_shooting_guards, 1))
+sf = np.poly1d(np.polyfit(years, OWS_sforwards, 1))
+pf = np.poly1d(np.polyfit(years, OWS_pforwards, 1))
+plt.plot(years,c(years),"b:")
+plt.plot(years,pg(years),"y:")
+#plt.plot(years,sg(years),"g:")
+#plt.plot(years,sf(years),"r:")
+#plt.plot(years,pf(years),"m:")
 
 #set up labels and legend
 plt.xlabel("NBA Season")
-plt.ylabel("# players in top 10 OWS")
+plt.ylabel("Winshares among top performers")
 plt.legend(loc=2)
+
+#path graph DWS vs OWS
+plt.subplot(2, 1, 2)
+plt.scatter(DWS_centers, OWS_centers, label="Centers", linewidth=2)
+plt.scatter(DWS_pgs,OWS_point_guards, label="Point Guards", linewidth=2)
+c = np.poly1d(np.polyfit(DWS_centers, OWS_centers, 1))
+pg = np.poly1d(np.polyfit(DWS_pgs, OWS_point_guards, 1))
+plt.plot(DWS_centers, c(DWS_centers), 'b:')
+plt.plot(DWS_pgs, pg(DWS_pgs), 'y:')
+plt.xlabel("Higher DWS →")
+plt.ylabel("Higher OWS →")
 plt.show()
